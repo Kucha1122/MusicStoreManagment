edf2c328fb4c2311fb7cbfc16a7ffdebd0cc2785
Kucha1122 <uyu1@O2.pl>
2018-05-11 19:38:07 +0200

    customerController

--- Store/Controllers/CustomerController.cs
+++ Store/Controllers/CustomerController.cs
@@ -1,11 +1,45 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Data.Interfaces;
+using Store.Models.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Store.Controllers
 {
-    public class CustomerController
+    public class CustomerController : Controller
     {
+        private readonly ICustomerRepository _customerRepository;
+        private readonly IAlbumRepository _albumRepository;
+
+        public CustomerController(ICustomerRepository customerRepository, IAlbumRepository albumRepository)
+        {
+            _customerRepository = customerRepository;
+            _albumRepository = albumRepository;
+        }
+
+        public IActionResult List()
+        {
+            var customerVM = new List<CustomerViewModel>();
+
+            var customers = _customerRepository.GetAll();
+
+            if(customers.Count() == 0)
+            {
+                return View("Empty");
+            }
+
+            foreach (var customer in customers)
+            {
+                customerVM.Add(new CustomerViewModel
+                {
+                    Customer = customer,
+                    AlbumCount = _albumRepository.Count(x => x.BorrowerId == customer.CustomerId)
+                });
+            }
+
+            return View(customerVM);
+        }
     }
 }
